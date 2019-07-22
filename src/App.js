5859e2a1e1434cdea3b17a7515a48086bfb55dc2
Remi Kristelijn <rkristelijn@gmail.com>
2019-07-22 13:21:06 +0200

    add dhx-suite

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout } from "dhx-suite";
+import "dhx-suite/codebase/suite.min.css";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  console.log(Layout);
+  var layout = new Layout("layout_container", {
+    cols: [
+      { header: "Column 1" },
+      {
+        header: "Column 2",
+        cols: [
+          { header: "Left cols" },
+          {
+            header: "Center cols",
+            cols: [
+              { header: "Sub Block Header cols 1" },
+              { header: "Sub Block Header cols 2" },
+              { header: "Sub Block Header cols 3" }
+            ]
+          }
+        ]
+      }
+    ]
+  });
+  return <div id="layout_container" />;
 }
 
 export default App;
