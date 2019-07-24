180c63e7b2a2aee6881916523c31e3e984af932e
Remi Kristelijn <rkristelijn@gmail.com>
2019-07-24 08:16:58 +0200

    tried grid

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import "./App.css";
 
 import Layout from "./dhx/Layout";
 import mainLayoutConfig from "./config/main-layout";
-import "dhx-suite/codebase/suite.min.css";
 
-// import Grid from "./dhx/Grid";
+import gridConfig from "./config/grid";
+import { Grid } from "dhx-suite";
+//import Grid from "./dhx/Grid";
+
+import "dhx-suite/codebase/suite.min.css";
 
 function App() {
   const layout = mainLayoutConfig;
-
+  // const grid = new Grid("grid_container", gridConfig);
   return (
     <Layout
       layout={layout}
       header={"HEAder"}
       sidebar={"SIDEbar"}
-      content={"COntent"}
+      content={new Grid("grid_container", gridConfig)}
       aside={"ASide"}
       footer={"FOoter"}
     />
---
+++ src/config/grid.js
@@ -0,0 +1,32 @@
+export default {
+  columns: [
+    { width: 100, id: "a", header: [{ text: "#" }] },
+    { width: 100, id: "b", header: [{ text: "Title" }] },
+    { width: 200, id: "c", header: [{ text: "Name" }] },
+    { width: 300, id: "d", header: [{ text: "Address" }] }
+  ],
+  headerRowHeight: 50,
+  data: [
+    {
+      id: 0,
+      a: 1,
+      b: "Linwood Long long long",
+      c: "Petersen",
+      d: "Dahlgreen Place"
+    },
+    {
+      id: 1,
+      a: 2,
+      b: "Edenburg",
+      c: "Agnes",
+      d: "Gem Street"
+    },
+    {
+      id: 2,
+      a: 3,
+      b: "Edenburg",
+      c: "Agnes",
+      d: "Gem Street"
+    }
+  ]
+};
